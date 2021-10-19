02f8510f6da7908bd0aa46777f365b4d9ce29e84
Karim Badwork <karim@badwork.fr>
2021-10-19 02:15:07 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,248 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WP_Bootstrap_Starter
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link href='https://encre-atelier.com/wp-content/themes/wp-bootstrap-starter-child/fonts/Metropolis-SemiBold.woff' rel='stylesheet' type='text/css'>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+    <!-- <link href="http://encre-atelier.com/wp-content/themes/wp-bootstrap-starter-child/reset.css" rel="stylesheet"> -->
+    <link href="https://encre-atelier.com/wp-content/themes/wp-bootstrap-starter-child/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="https://encre-atelier.com/wp-content/themes/wp-bootstrap-starter-child/owlcarousel/assets/owl.theme.default.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://encre-atelier.com/wp-content/themes/wp-bootstrap-starter-child/zoom-img/css/image-zoom.css">
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+<?php 
+
+    // WordPress 5.2 wp_body_open implementation
+    if ( function_exists( 'wp_body_open' ) ) {
+        wp_body_open();
+    } else {
+        do_action( 'wp_body_open' );
+    }
+
+?>
+
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'wp-bootstrap-starter' ); ?></a>
+    <?php if(!is_page_template( 'blank-page.php' ) && !is_page_template( 'blank-page-with-container.php' )): ?>
+	<header id="masthead" class="site-header navbar-static-top <?php echo wp_bootstrap_starter_bg_class(); ?>" role="banner">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="blue-banner">
+                        <p>FREE SHIPPING FOR DELIVERY OVER 250€ - FREE SHIPPING FOR DELIVERY OVER 250€ - FREE SHIPPING FOR DELIVERY OVER 250€ - FREE SHIPPING FOR DELIVERY OVER 250€</p>
+                    </div>
+                </div>
+                <div class="col-2 logo-container">
+                    <img src="https://encre-atelier.com/wp-content/uploads/2021/04/encre-black.svg">
+                </div>
+                <div class="col-10 nav-container">
+                    <nav class="navbar">
+
+                        <?php
+                        wp_nav_menu(array(
+                        'theme_location'    => 'primary',
+                        'container'       => 'div',
+                        'container_id'    => 'main-nav',
+                        'container_class' => 'navbar-collapse justify-content-center',
+                        'menu_id'         => false,
+                        'menu_class'      => 'navbar-nav',
+                        'depth'           => 3,
+                        'fallback_cb'     => 'wp_bootstrap_navwalker::fallback',
+                        'walker'          => new wp_bootstrap_navwalker()
+                        ));
+                        ?>
+
+                    </nav>
+                    <div class="black-banner">
+                    <p>FREE SHIPPING FOR DELIVERY OVER 250€ - FREE SHIPPING FOR DELIVERY OVER 250€ - FREE SHIPPING FOR DELIVERY OVER 250€ - FREE SHIPPING FOR DELIVERY OVER 250€</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container d-none">
+            <div class="row">
+                <div class="col-7 col-md-3">
+                    <button class="btn navigation-mobile d-md-none" type="button" data-toggle="modal" data-target="#menuModal">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 7 38">
+                            <g id="burger-menu-groupe" data-name="Calque 2">
+                                <g id="Calque_1-2" data-name="Calque 1">
+                                    <circle id="clothingDot" cx="3.5" cy="3.5" r="3.5"/>
+                                    <circle id="homeDot" cx="3.5" cy="13.5" r="3.5"/>
+                                    <circle id="labDot" cx="3.5" cy="24.5" r="3.5"/>
+                                    <circle id="aboutDot" cx="3.5" cy="34.5" r="3.5"/>
+                                </g>
+                            </g>
+                        </svg>
+                    </button>
+                    <div class="logoContainer">
+                        <!-- <a href="https://encre-atelier.com/fr" class="header-logo lang_fr">Encré.</a>
+                        <a href="https://encre-atelier.com/en" class="header-logo lang_en">Encré.</a> -->
+                        <a href="https://encre-atelier.com/fr" class="header-logo lang_fr">
+                            <img src="https://encre-atelier.com/wp-content/uploads/2021/04/encre-black.svg">
+                        </a>
+                        <a href="https://encre-atelier.com/en" class="header-logo lang_en">
+                            <img src="https://encre-atelier.com/wp-content/uploads/2021/04/encre-black.svg">
+                        </a>
+                        <p id="subheader-logo">&nbsp;</p>
+                    </div>
+                </div>
+                <div class="col-1 col-md-6 d-none d-sm-block">
+                    <!-- <nav class="navbar navbar-expand-xl p-0">
+                        <ul class="navbar-nav">
+                            <li class="nav-item"><a href="" class="nav-link">Clothing.</a></li>
+                            <li class="nav-item"><a href="" class="nav-link">Home.</a></li>
+                            <li class="nav-item"><a href="" class="nav-link">Lab.</a></li>
+                            <li class="nav-item"><a href="" class="nav-link">About.</a></li>
+                        </ul>
+                    </nav> -->
+                    <nav class="navbar navbar-expand-xl p-0">
+                        <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-nav" aria-controls="" aria-expanded="false" aria-label="Toggle navigation">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 7 38">
+                            <g id="burger-menu-groupe" data-name="Calque 2">
+                                <g id="Calque_1-2" data-name="Calque 1">
+                                    <circle id="clothingDot" cx="3.5" cy="3.5" r="3.5"/>
+                                    <circle id="homeDot" cx="3.5" cy="13.5" r="3.5"/>
+                                    <circle id="labDot" cx="3.5" cy="24.5" r="3.5"/>
+                                    <circle id="aboutDot" cx="3.5" cy="34.5" r="3.5"/>
+                                </g>
+                            </g>
+                        </svg>
+                        </button> -->
+
+                        <?php
+                        wp_nav_menu(array(
+                        'theme_location'    => 'primary',
+                        'container'       => 'div',
+                        'container_id'    => 'main-nav',
+                        'container_class' => 'navbar-collapse justify-content-center',
+                        'menu_id'         => false,
+                        'menu_class'      => 'navbar-nav',
+                        'depth'           => 3,
+                        'fallback_cb'     => 'wp_bootstrap_navwalker::fallback',
+                        'walker'          => new wp_bootstrap_navwalker()
+                        ));
+                        ?>
+
+                    </nav>
+                </div>
+                <div class="col-2 col-md-2">
+                    <div class="search-link text-right">
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-recherche" data-toggle="modal" data-target="#searchModal">
+                            <img src="https://encre-atelier.com/wp-content/uploads/2021/03/eye-close.svg" class="search-svg"/>
+                        </button>
+                    </div>
+                </div>
+                <div class="col-2 col-md-1">
+                    <div class="header-cart text-right">
+                        <span id="badgeCart"></span>
+                        <a href="https///encre-atelier.com/fr/cart" class="btn lang_fr">
+                            <svg class="panier-svg" xmlns="http://www.w3.org/2000/svg" width="41.855" height="43.849" viewBox="0 0 41.855 43.849">
+                                <g id="Groupe_208" data-name="Groupe 208" transform="translate(-1739.145 -90.291)">
+                                    <g id="Tracé_2" data-name="Tracé 2" transform="translate(1739.145 100.258)" fill="none">
+                                    <path d="M2.529,0H39.248l2.607,33.882H0Z" stroke="none"/>
+                                    <path d="M 4.385108947753906 1.999996185302734 L 2.154830932617188 31.88241577148438 L 39.6949462890625 31.88241577148438 L 37.39557647705078 1.999996185302734 L 4.385108947753906 1.999996185302734 M 2.528820037841797 -3.814697265625e-06 L 39.24760055541992 -3.814697265625e-06 L 41.85475921630859 33.88241577148438 L 0 33.88241577148438 L 2.528820037841797 -3.814697265625e-06 Z" stroke="none" fill="#000"/>
+                                    </g>
+                                    <g id="Soustraction_1" data-name="Soustraction 1" transform="translate(1747.149 90.291)" fill="none">
+                                    <path d="M25.845,11.959H0A12.7,12.7,0,0,1,4.054,3.475a12.989,12.989,0,0,1,21.791,8.482Z" stroke="none"/>
+                                    <path d="M 23.48784065246582 9.958898544311523 C 22.21307754516602 5.35752010345459 17.9284782409668 1.999998807907104 12.92341899871826 1.999998807907104 C 10.12105846405029 1.999998807907104 7.454988956451416 3.044058799743652 5.416318893432617 4.939848899841309 C 3.931335926055908 6.32074499130249 2.881961584091187 8.055333137512207 2.356542348861694 9.958898544311523 L 23.48784065246582 9.958898544311523 M 25.84527969360352 11.95889854431152 L 25.84342956542969 11.95889854431152 L -1.106872559830663e-06 11.95889854431152 C 0.2154488861560822 8.717808723449707 1.65592885017395 5.705558776855469 4.05436897277832 3.475238800048828 C 6.464308738708496 1.234188795089722 9.614078521728516 -1.204681439048727e-06 12.92341899871826 -1.204681439048727e-06 C 19.7228889465332 -1.204681439048727e-06 25.39878845214844 5.25217866897583 25.84515953063965 11.95706844329834 L 25.84527969360352 11.95889854431152 Z" stroke="none" fill="#000"/>
+                                    </g>
+                                    <path id="Tracé_14" data-name="Tracé 14" d="M88.982,147.276a4.366,4.366,0,0,0-3.806,2.227,4.366,4.366,0,0,0-3.806-2.227A4.62,4.62,0,0,0,77,151.928c0,6.061,8.176,10.29,8.176,10.29s8.176-4.229,8.176-10.29a4.62,4.62,0,0,0-4.37-4.652Zm0,0" transform="translate(1674.896 -37.547)" fill="#fa4825"/>
+                                </g>
+                            </svg>
+                        </a>
+                        <a href="https///encre-atelier.com/en/cart" class="btn lang_en">
+                            <svg class="panier-svg" xmlns="http://www.w3.org/2000/svg" width="41.855" height="43.849" viewBox="0 0 41.855 43.849">
+                                <g id="Groupe_208" data-name="Groupe 208" transform="translate(-1739.145 -90.291)">
+                                    <g id="Tracé_2" data-name="Tracé 2" transform="translate(1739.145 100.258)" fill="none">
+                                    <path d="M2.529,0H39.248l2.607,33.882H0Z" stroke="none"/>
+                                    <path d="M 4.385108947753906 1.999996185302734 L 2.154830932617188 31.88241577148438 L 39.6949462890625 31.88241577148438 L 37.39557647705078 1.999996185302734 L 4.385108947753906 1.999996185302734 M 2.528820037841797 -3.814697265625e-06 L 39.24760055541992 -3.814697265625e-06 L 41.85475921630859 33.88241577148438 L 0 33.88241577148438 L 2.528820037841797 -3.814697265625e-06 Z" stroke="none" fill="#000"/>
+                                    </g>
+                                    <g id="Soustraction_1" data-name="Soustraction 1" transform="translate(1747.149 90.291)" fill="none">
+                                    <path d="M25.845,11.959H0A12.7,12.7,0,0,1,4.054,3.475a12.989,12.989,0,0,1,21.791,8.482Z" stroke="none"/>
+                                    <path d="M 23.48784065246582 9.958898544311523 C 22.21307754516602 5.35752010345459 17.9284782409668 1.999998807907104 12.92341899871826 1.999998807907104 C 10.12105846405029 1.999998807907104 7.454988956451416 3.044058799743652 5.416318893432617 4.939848899841309 C 3.931335926055908 6.32074499130249 2.881961584091187 8.055333137512207 2.356542348861694 9.958898544311523 L 23.48784065246582 9.958898544311523 M 25.84527969360352 11.95889854431152 L 25.84342956542969 11.95889854431152 L -1.106872559830663e-06 11.95889854431152 C 0.2154488861560822 8.717808723449707 1.65592885017395 5.705558776855469 4.05436897277832 3.475238800048828 C 6.464308738708496 1.234188795089722 9.614078521728516 -1.204681439048727e-06 12.92341899871826 -1.204681439048727e-06 C 19.7228889465332 -1.204681439048727e-06 25.39878845214844 5.25217866897583 25.84515953063965 11.95706844329834 L 25.84527969360352 11.95889854431152 Z" stroke="none" fill="#000"/>
+                                    </g>
+                                    <path id="Tracé_14" data-name="Tracé 14" d="M88.982,147.276a4.366,4.366,0,0,0-3.806,2.227,4.366,4.366,0,0,0-3.806-2.227A4.62,4.62,0,0,0,77,151.928c0,6.061,8.176,10.29,8.176,10.29s8.176-4.229,8.176-10.29a4.62,4.62,0,0,0-4.37-4.652Zm0,0" transform="translate(1674.896 -37.547)" fill="#fa4825"/>
+                                </g>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!-- Modal -->
+        <div class="modal fade" id="menuModal" tabindex="-1" aria-labelledby="menuModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <i class="fas fa-times"></i>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="nav-links lang_fr">
+                            <a href="https://encre-atelier.com/fr/clothing/?swoof=1&product_cat=new-in">Clothing.</a>
+                            <a href="https://encre-atelier.com/fr/home">Home.</a>
+                            <a href="https://encre-atelier.com/fr/lab">Lab.</a>
+                            <a href="https://encre-atelier.com/fr/about">About.</a>
+                        </div>
+                        <div class="nav-links lang_en">
+                            <a href="https://encre-atelier.com/en/clothing/?swoof=1&product_cat=new-in">Clothing.</a>
+                            <a href="https://encre-atelier.com/en/home">Home.</a>
+                            <a href="https://encre-atelier.com/en/lab">Lab.</a>
+                            <a href="https://encre-atelier.com/en/about">About.</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+	</header><!-- #masthead -->
+    <?php if(is_front_page() && !get_theme_mod( 'header_banner_visibility' )): ?>
+        <div id="page-sub-header" <?php if(has_header_image()) { ?>style="background-image: url('<?php header_image(); ?>');" <?php } ?>>
+            <div class="container">
+                <h1>
+                    <?php
+                    if(get_theme_mod( 'header_banner_title_setting' )){
+                        echo esc_attr( get_theme_mod( 'header_banner_title_setting' ) );
+                    }else{
+                        echo 'WordPress + Bootstrap';
+                    }
+                    ?>
+                </h1>
+                <p>
+                    <?php
+                    if(get_theme_mod( 'header_banner_tagline_setting' )){
+                        echo esc_attr( get_theme_mod( 'header_banner_tagline_setting' ) );
+                }else{
+                        echo esc_html__('To customize the contents of this header banner and other elements of your site, go to Dashboard > Appearance > Customize','wp-bootstrap-starter');
+                    }
+                    ?>
+                </p>
+                <a href="#content" class="page-scroller"><i class="fa fa-fw fa-angle-down"></i></a>
+            </div>
+        </div>
+    <?php endif; ?>
+	<div id="content" class="site-content">
+		<div class="container">
+			<div class="row">
+                <?php endif; ?>
